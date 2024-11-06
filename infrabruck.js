9cef8bdd9aebcb1b3e6ecdf86bb4fbc3a6b30642
xelaoliver <emmet2by4@gmail.com>
2024-11-06 17:02:40 +0000

    Update infrabruck.js

--- infrabruck.js
+++ infrabruck.js
@@ -9,7 +9,7 @@ function brackets(code) {
 
       while (open > 0 && bracketsIndex < code.length) {
         bracketsIndex++;
-        
+       
         if (code[bracketsIndex] === "[") {
           open ++;
         } else if (code[bracketsIndex] === "]") {
@@ -25,5 +25,40 @@ function brackets(code) {
   return brackets;
 }
 
-brackets = brackets("x[xx[x]x]xx");
-console.log(brackets);
+function arithmatic(brackets, code) {
+  var pointer = 0;
+  var cells = new Array(64).fill(0);
+  var index = 0;
+
+  while (index < code.length) {
+    if (code[index] === ">") {
+      pointer++;
+    } else if (code[index] === "<") {
+      pointer--;
+    } else if (code[index] === "+") {
+      cells[pointer] = (cells[pointer] + 1) % 256; // Wrap around at 256
+    } else if (code[index] === "-") {
+      cells[pointer] = (cells[pointer] - 1 + 256) % 256; // Wrap around at 256
+    } else if (code[index] === "[") {
+      if (cells[pointer] === 0) {
+        index = brackets[brackets.indexOf(index) + 1]; // Jump to the matching closing bracket
+      }
+    } else if (code[index] === "]") {
+      if (cells[pointer] !== 0) {
+        index = brackets[brackets.indexOf(index) - 1]; // Jump back to the matching opening bracket
+      }
+    } else if (code[index] == ".") {
+      output.push(String.fromCharCode(cells[pointer]));
+    }
+    index++;
+  }
+  return cells;
+}
+
+let code = "+++++++++++++[>+++++<-]>.";
+
+var brackets = brackets(code);
+
+var output = [];
+var cells = arithmatic(brackets, code);
+document.write("code: "+code+"<br><br>"+"cells: "+cells+"<br><br>"+"out: "+output);
